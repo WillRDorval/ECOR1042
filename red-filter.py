711ebb27dbcec44d67bd2f5d9abc940b8a2bfa20
William Dorval <will.rome.dorval@gmail.com>
2021-01-21 10:45:17 -0500

    added docstring

--- red-filter.py
+++ red-filter.py
@@ -1,7 +1,13 @@
-from Cimpl import choose_file, load_image, copy, create_color, set_color,show, Image, get_color
+from Cimpl import choose_file, load_image, copy, create_color, set_color, show, Image, get_color
 
 
 def red_channel(image: Image) -> Image:
+    """
+    Creates an image containing only the red channel of the given image.
+    >>> test_image = load_image(choose_file())
+    >>> test_result = red_channel(test_image)
+    >>> show(test_result)
+    """
     new_image = copy(image)
     for pixel in new_image:
         x, y, (r, g, b) = pixel
