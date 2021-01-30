c2edadadcd20957b1d86c8fe0d27567bc4a2684c
Raunaq Hoque <raunaq.m.h@gmail.com>
2021-01-31 01:35:34 +0800

    Final

--- T099_P3_posterize.py
+++ T099_P3_posterize.py
@@ -1,17 +1,18 @@
 from Cimpl import choose_file, load_image, copy, create_color, set_color,\
                   show, Image, get_color, create_image, Color
 
-def _adjust_component (a, b, c):
+def _adjust_component (r, g, b):
+    p_col = [r,g,b]
     for i in range(3):
-        if ((r,g,b)[i] <= 63):
-            (r,g,b)[i] = 31
-        elif ((r,g,b)[i] >= 64) and ((r,g,b)[i] <= 127):
-            (r,g,b)[i] = 95
-        elif ((r,g,b)[i] >= 128) and ((r,g,b)[i] <= 191):
-            (r,g,b)[i] = 159
-        elif ((r,g,b)[i] >= 192) and ((r,g,b)[i] <= 255):
-            (r,g,b)[i] = 223 
-        pixel = create_color(r, g, b)
+        if p_col[i] <= 63:
+            p_col[i] = 31
+        elif p_col[i] <= 127:
+            p_col[i] = 95
+        elif  p_col[i] <= 191:
+            p_col[i] = 159
+        elif p_col[i] <= 255:
+            p_col[i] = 223 
+        pixel = create_color(*p_col)
     return pixel    
 
 def posterize(image: Image) -> Image:
