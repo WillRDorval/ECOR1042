f2f1d862721ffd7317c2adc57d6150cc23449afe
ChaelanMurray <77498223+ChaelanMurray@users.noreply.github.com>
2021-01-31 10:58:32 -0500

    Add files via upload

--- T099_P3_filter_extreme.py
+++ T099_P3_filter_extreme.py
@@ -1,26 +1,25 @@
 from Cimpl import *
 
-def extreme_contrast((image: Image) -> Image :
+def extreme_contrast(image: Image) -> Image :
     """ Returns a copy of an image with the contrast between the pixels being maximized. 
     >>> image = load_image(choose_file())
     >>> extreme_image = extreme_contrast(image)
     >>> show(extreme_image)
     """
     new_image = copy(image)
-    red, green, blue = (r, g, b)
     for x, y, (r, g, b) in image:
-        extreme = create_color(r, g, b)
-        if red >= 127:
-            red = 0
-        elif red <= 128:
-            red = 255
-        if green >= 127:
-            green = 0
-        elif green <= 128:
-            green = 255   
-        if blue >= 127:
-                blue = 0
-        elif blue <= 128:
-                blue = 255                 
+        col = [r, g, b] 
+        for t in range(3):
+            if col[t] >= 128:
+                col[t] = 255
+            else: 
+                col[t] = 0      
+        extreme = create_color(*col)
         set_color(new_image, x, y, extreme)       
     return new_image    
+
+# Main Script 
+show (extreme_contrast(load_image(choose_file())))
+
+
+print("End of file reached successfully.")
