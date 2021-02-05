0c70953d19d6c7cb5d776710160f7f64f5b341a0
William Dorval <will.rome.dorval@gmail.com>
2021-02-05 09:55:54 -0500

    added actual edge detection filter for evaluation

--- T099_P4_filter_edge.py
+++ T099_P4_filter_edge.py
@@ -9,15 +9,26 @@ from Cimpl import choose_file, load_image, copy, create_color, set_color, show, 
 import math
 
 
-def accurate_grayscale(input_image: Image) -> Image:
+def grayscale(input_image: Image) -> Image:
     result = copy(input_image)
-    for x, y, (r, g, b) in result:
-        gray = round(0.299 * r + 0.587 * g + 0.114 * b)
-        set_color(result, x, y, create_color(gray, gray, gray))
+    for x, y, (r, g, b) in input_image:
+        gray = (r+g+b)/3
+        result.set_color(x, y, create_color(gray, gray, gray))
+
     return result
 
 
-def detect_edges(input_image: Image, tolerance: int) -> Image:
+def detect_edges(input_image: Image, threshold: int) -> Image:
+    brightness = grayscale(input_image)
+    result = Image(width=input_image.get_width(), height=input_image.get_height())
+    for x, y, (r, _, _) in brightness:
+        if abs(r - brightness.get_color(x, y-1)[0]) > threshold:
+            result.set_color(x, y, create_color(0, 0, 0))
+
+    return result
+
+
+def reaL_detect_edges(input_image: Image, tolerance: int) -> Image:
     if 100 < tolerance < 0:
         raise ValueError("tolerance must be a percentage form 0 to 100")
 
@@ -83,44 +94,13 @@ def detect_edges(input_image: Image, tolerance: int) -> Image:
                 pass
 
             else:
-                """if r_diff > g_diff and r_diff > b_diff:
-                    val = 255 * ((r_diff - gap) / (1 - gap))
-                elif g_diff > b_diff:
-                    val = 255 * ((g_diff - gap) / (1 - gap))
-                else:
-                    val = 255 * ((b_diff - gap) / (1 - gap))
-                """
                 diff = (0.299 * r_diff + 0.587 * g_diff + 0.114 * b_diff)
                 val = 255 - round(255 * (math.sqrt(diff)/(1 + pow(math.e, -8*(diff-gap)))))
                 result.set_color(x, y, create_color(val, val, val))
     return result
 
 
-"""
-def cleanup_edges(input_image: Image) -> Image:
-    result = copy(input_image)
-    thickness = [0]*input_image.get_width()
-    for x in range(input_image.get_width()):
-        for y in range(input_image.get_height()):
-            if input_image.get_color(x, y) == create_color(0, 0, 0):
-                thickness[x] = thickness[x] + 1
-            else:
-                if thickness[x] > 4:
-                    if thickness[x] & 1 == 1:
-                        center = y - math.ceil(thickness[x]/2)
-                        for i in range(y-thickness[x], y):
-                            if abs(center-i) > 1:
-                                result.set_color(x, i, create_color(255, 255, 255))
-                    else:
-                        center = y - (thickness[x]/2 + 0.5)
-                        for i in range(y-thickness[x], y):
-                            if abs(center-i) >= 2:
-                                result.set_color(x, i, create_color(255, 255, 255))
-                thickness[x] = 0
-    return result
-"""
-
 if __name__ == "__main__":
-    image = detect_edges(load_image(choose_file()), 30)
+    image = detect_edges(load_image(choose_file()), 40)
     show(image)
     save_as(image)
