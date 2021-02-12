064fe1a703ab479b16c387d42b23c06130989b01
SAUD <immsaud@gmail.com>
2021-02-12 19:31:51 +0300

    no message

---
+++ T099_batch_ui.py
@@ -0,0 +1,35 @@
+"""
+Submission by T099
+
+William Dorval 101187466
+Raunaq Hoqie 101180524
+Chaelan Murray 101180990
+Mohammad Saud 101195172
+
+Submitted on: 
+"""
+
+from T099_interactive_ui import command
+from Cimpl import choose_file, load_image, copy, create_color, set_color, show, Image, save_as, get_color, create_image
+
+filename = "batch_sample.txt"
+batch_file = open(filename)
+
+for line in batch_file:
+    
+    items = line.split(" ")
+    filter_list = [""] * (len(items)-1)
+    for i in range(2,len(filter_list)):
+        filter_list.append(item[i])
+    
+    current_image = load_image(item[0])
+    for filter_entry in filter_list:
+        new_image = command(filter_entry, current_image)
+        current_image = copy(new_image)
+        
+    final_image_name = item[1]
+    save_as(current_image, final_image)
+    show(current_image)
+        
+    
+    
